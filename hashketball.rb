c33d16c7c6a451e642fc46e8401bd841badc519a
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 05:29:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,7 +101,7 @@ def game_hash
           :slam_dunks => 5
         },
         {
-          :player_name => ,
+          :player_name => "Ben Gordon",
           :number  => ,
           :shoe => ,
           :points => ,
