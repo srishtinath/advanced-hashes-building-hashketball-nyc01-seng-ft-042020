da83720a0f6c5a14975005056da8c397684e0be9
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 05:30:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,7 +79,7 @@ def game_hash
           :slam_dunks => 2
         },
         {
-          :player_name => "Bismack Boyombo",
+          :player_name => "Bismack Biyombo",
           :number => 0,
           :shoe => 16,
           :points => 12,
