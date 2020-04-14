2c46576c3a773c089aa904bf3da8679120cd4cee
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 05:28:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,15 +68,15 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
-          :player_name => "Alan Anderson",
-          :number  => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
         },
         {
           :player_name => ,
