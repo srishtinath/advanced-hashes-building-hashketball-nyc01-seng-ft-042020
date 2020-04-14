4e5f2172e372d9494d59c5a17d50c3926ca18ec4
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 05:21:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
     :home =>
     {
       :team_name => "Brooklyn Nets",
-      :colors => ["black", "white"],
+      :colors => ["Black", "White"],
       :players => [
         {:player_name => ,
         :number  => ,
