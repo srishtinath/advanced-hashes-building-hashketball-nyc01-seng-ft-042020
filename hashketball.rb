69d6b0308fcad722930087fec3b8dec3daf6a824
Srishti Nath <srishti.nath@gmail.com>
2020-04-14 05:28:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,7 +90,7 @@ def game_hash
           :slam_dunks => 10
         },
         {
-          :player_name => ,
+          :player_name => "DeSagna Diop",
           :number  => ,
           :shoe => ,
           :points => ,
